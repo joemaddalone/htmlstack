6db5779c25f53c834a7f79c3d45cade4161d3d4d
Joe Maddalone <joe@insert-title.com>
2017-04-24 13:49:53 -0500

    update mean half second timer

--- src/triangle/js/game2017.js
+++ src/triangle/js/game2017.js
@@ -369,7 +369,7 @@ game.prototype = {
             if ( +timer.textContent == 0 ) {
                 self.lose()
             }
-        }, 500 )
+        }, 1000 )
     },
     lose: function () {
         /*
@@ -402,17 +402,3 @@ game.prototype = {
         return me
     }
 }
-
-// (function () {
-//     /*
-//      sets up Rules presentation onLoad
-//      */
-//     _('hideRules').onclick = function () {
-//         setClass('rules', 'slideOut');
-//         setClass('getRules', 'slideIn')
-//     };
-//     _('getRules').onclick = function () {
-//         setClass('rules', 'slideIn');
-//         setClass('getRules', 'slideOut')
-//     }
-// })();
