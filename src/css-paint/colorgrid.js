add7ec30bfaf5e17578ef4a23302309806c1d59e
Joe Maddalone <jmaddalone@cloudbees.com>
2018-03-04 12:22:28 -0600

    css paint api

---
+++ src/css-paint/colorgrid.js
@@ -0,0 +1,19 @@
+class ColorGrid {
+	paint(ctx, geom, properties) {
+		const random_color = () => {
+			let rint = Math.round(0xffffff * Math.random());
+			return `rgb(${(rint >> 16)}, ${(rint >> 8 & 255)}, ${(rint & 255)})`;
+		}
+		const size = 50;
+		for (let y = 0; y < geom.height / size; y++) {
+			for (let x = 0; x < geom.width / size; x++) {
+				ctx.beginPath();
+				ctx.fillStyle = random_color();
+				ctx.rect(x * size, y * size, size, size);
+				ctx.fill();
+			}
+		}
+	}
+}
+
+registerPaint('checkerboard', ColorGrid);
