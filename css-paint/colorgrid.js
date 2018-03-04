a9c08e4781abeebbc665d2891c6a31acc7554fa1
Joe Maddalone <jmaddalone@cloudbees.com>
2018-03-04 12:23:37 -0600

    Updates

---
+++ css-paint/colorgrid.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ColorGrid = function () {
+	function ColorGrid() {
+		_classCallCheck(this, ColorGrid);
+	}
+
+	_createClass(ColorGrid, [{
+		key: 'paint',
+		value: function paint(ctx, geom, properties) {
+			var random_color = function random_color() {
+				var rint = Math.round(0xffffff * Math.random());
+				return 'rgb(' + (rint >> 16) + ', ' + (rint >> 8 & 255) + ', ' + (rint & 255) + ')';
+			};
+			var size = 50;
+			for (var y = 0; y < geom.height / size; y++) {
+				for (var x = 0; x < geom.width / size; x++) {
+					ctx.beginPath();
+					ctx.fillStyle = random_color();
+					ctx.rect(x * size, y * size, size, size);
+					ctx.fill();
+				}
+			}
+		}
+	}]);
+
+	return ColorGrid;
+}();
+
+registerPaint('checkerboard', ColorGrid);
